57003a23595fef80cd9366be54e458786bebec0f
QuanggChienn <pchien23012004@gmail.com>
2025-06-06 23:51:49 +0700

    them entity giay, danh muc, danh muc con

---
+++ src/main/java/com/project/moveupweb/entities/Giay.java
@@ -0,0 +1,53 @@
+package com.project.moveupweb.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "giay")
+public class Giay {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ten_san_pham")
+    private String tenSanPham;
+
+    @Column(name = "mo_ta_san_pham")
+    private String moTaSanPham;
+
+    @Column(name = "trang_thai")
+    private Boolean trangThai;
+
+    @OneToOne
+    @JoinColumn(name = "id_danh_muc", referencedColumnName = "id")
+    private DanhMuc danhMuc;
+
+    @OneToOne
+    @JoinColumn(name = "id_danh_muc_con", referencedColumnName = "id")
+    private DanhMucCon danhMucCon;
+
+    @Column(name = "thuong_hieu")
+    private String thuongHieu;
+
+    @Column(name = "chat_lieu")
+    private String chatLieu;
+
+    @Column(name = "xuat_xu")
+    private String xuatXu;
+
+    @Column(name = "kieu_dang")
+    private String kieuDang;
+
+    @Column(name = "tu_khoa")
+    private String tuKhoa;
+
+    @Column(name = "uu_tien")
+    private Integer uuTien;
+
+}
